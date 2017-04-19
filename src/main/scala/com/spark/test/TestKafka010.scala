516f65867cdcfb9b5ed1b9ad68acac52e8ffa86d
LMQ <LMQ@zhiziyun-PC>
2017-04-19 09:20:55 +0800

    ss

--- src/main/scala/com/spark/test/TestKafka010.scala
+++ src/main/scala/com/spark/test/TestKafka010.scala
@@ -6,12 +6,12 @@ import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.Seconds
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.spark.streaming.kafka.KafkaUtils
+//import org.apache.spark.streaming.kafka.KafkaUtils
 
-/*import org.apache.spark.streaming.kafka010.KafkaUtils*/
-/*import org.apache.spark.streaming.kafka010._*/
-/*import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
-import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe*/
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 object TestKafka010 {
   val kafkaParams = Map[String, Object](
   "bootstrap.servers" -> "192.168.10.84:9092,192.168.10.85:9092,192.168.10.86:9092",
@@ -23,12 +23,11 @@ object TestKafka010 {
 )
 val topics = Array("realtimereport_box")
  def main(args: Array[String]): Unit = {
-    val sc=new SparkContext(new SparkConf().setMaster("local").setAppName("Test"))
-    
+ 
     
   }
 
-/*def kafka010(){
+def kafka010(){
      val sc=new SparkContext(new SparkConf().setMaster("local").setAppName("Test"))
     val ssc=new StreamingContext(sc,Seconds(5))
     val r=KafkaUtils.createDirectStream(
@@ -40,5 +39,5 @@ val topics = Array("realtimereport_box")
     ssc.start()
     ssc.awaitTermination()
        
-  }*/
+  }
 }
