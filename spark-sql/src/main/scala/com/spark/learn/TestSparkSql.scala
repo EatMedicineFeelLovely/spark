d5dfdec7bf51cd0bbcb3a4bf50650a5a7a58fb75
linmingqiang <1356469429@qq.com>
2019-08-20 14:37:01 +0800

    left join

--- spark-sql/src/main/scala/com/spark/learn/TestSparkSql.scala
+++ spark-sql/src/main/scala/com/spark/learn/TestSparkSql.scala
@@ -4,6 +4,7 @@ import org.apache.spark.sql.SparkSession
 import com.spark.learn.cassclass.kv
 import com.spark.learn.udf.SparkSqlMaxUDFA
 import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.sql.functions._
 object TestSparkSql {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
@@ -11,8 +12,28 @@ object TestSparkSql {
       .appName("test")
       .master("local")
       .getOrCreate()
+    import spark.implicits._
+
+    leftJoinTest(spark)
     //arrayColumns(spark)
-    udfTest(spark)
+    // udfTest(spark)
+  }
+
+  /**
+    *
+    * @param spark
+    */
+  def leftJoinTest(spark: SparkSession): Unit = {
+    import spark.implicits._
+    spark.sparkContext
+      .parallelize(1 to 10)
+      .toDF("b")
+      .join(spark.sparkContext.parallelize(Array((1, 2))).toDF("a", "c"),
+            expr("b = c"),
+            "leftouter")
+      .filter($"c".isNull)
+      .show
+
   }
 
   /**
@@ -27,9 +48,9 @@ object TestSparkSql {
     * @param broadcast
     * @return
     */
-  def getBrocastUDF(broadcast: Broadcast[Array[Int]]): (Int) => String = {
+  def getBrocastUDF(broadcast: Broadcast[Array[Int]]): (Int) => Array[Int] = {
     (a: Int) =>
-      broadcast.value.mkString(",") + "," + a
+      broadcast.value
   }
   case class ArrayBean(a: Array[Int], b: String)
 
@@ -42,7 +63,7 @@ object TestSparkSql {
     spark.sparkContext.setLogLevel("ERROR")
     val broadcast = spark.sparkContext.broadcast(Array(1, 2, 3, 4))
     spark.sparkContext
-      .parallelize(1 to 100)
+      .parallelize(1 to 10)
       .map(x => kv(x / 5, x))
       .toDF()
       .cache()
@@ -61,7 +82,7 @@ object TestSparkSql {
                  |FROM testTable
                  |WHERE k <100
                  |group by k
-      """.stripMargin).show
+      """.stripMargin).withColumn("ex", explode($"brocast_udf")).show
   }
 
   /**
