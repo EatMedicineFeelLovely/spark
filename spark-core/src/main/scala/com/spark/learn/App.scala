bbfeaa5113f34667f2298c431fdd9d655eb94c65
LinMingQiang <1356469429@qq.com>
2019-11-10 11:48:07 +0800

    core sql pom

--- spark-core/src/main/scala/com/spark/learn/App.scala
+++ spark-core/src/main/scala/com/spark/learn/App.scala
@@ -1,15 +1,23 @@
 package com.spark.learn
 
+import org.apache.spark.{HashPartitioner, SparkContext}
+
 /**
  * @author ${user.name}
  */
 object App {
   
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
-  def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+
+  def main(args : Array[String]): Unit = {
+    val sc = new SparkContext()
+
+    val a = sc.parallelize(1 to 10,10)
+    val b =a.zip(a).partitionBy(new HashPartitioner(10))
+b.join(b)
+    a.foreachPartition(x=>x.foreach(println))
+
+    b.foreachPartition(x=>x.foreach(println))
+
   }
 
 }
--- spark-sql/src/main/scala/com/spark/learn/cassclass/package.scala
+++ spark-sql/src/main/scala/com/spark/learn/cassclass/package.scala
@@ -1,5 +1,4 @@
 package com.spark.learn
 
 package object cassclass {
-  case class kv(k: Int, v: Int) extends AbstractBasicsCaseClass[kv]
 }
