ee3ce99842e944ecbeb2012672144296f00f9713
mqlin <1356469429@qq.com>
2019-11-30 22:43:28 +0800

    自定义DataType（UDT）；利用HyperLog计算UV（适合流中粗略计算）

--- spark-sql/src/test/scala/UDTTest.scala
+++ spark-sql/src/test/scala/UDTTest.scala
@@ -44,49 +44,54 @@ object UDTTest {
       }
 
     /**
-     * +---+---+---+
-     * |key|pv |uv |
-     * +---+---+---+
-     * |b  |3  |1  |
-     * |a  |3  |2  |
-     * +---+---+---+
-     */
+      * +---+---+---+
+      * |key|pv |uv |
+      * +---+---+---+
+      * |b  |3  |1  |
+      * |a  |3  |2  |
+      * +---+---+---+
+      */
     df1.foreach(println)
-    /**
-     * +---+---+---+
-     * |key|pv |uv |
-     * +---+---+---+
-     * |b  |6  |1  |
-     * |a  |6  |2  |
-     * +---+---+---+
-     */
-    df1.union(df1).groupBy(_._1).mapPartitions { itor =>
-      itor.map {
-        case (uid, l) =>
-          val list = l.toList
-          var (_, pv, uv) = list.head
-          for(i <- 1 to list.size-1){
-            val (_,p,u) = list(i)
-            pv+=p
-            uv.addAll(u)
-          }
-          (uid, pv , uv.cardinality())
-      }
-    }
-    .toDF("key", "pv", "uv")
-    .show(false)
 
     /**
-     * +---+---+---+
-     * |key| pv| uv|
-     * +---+---+---+
-     * |  b|  3|  1|
-     * |  a|  3|  2|
-     * +---+---+---+
-     */
-        spark
-          .sql(
-            s"select key,count(uid) pv,count(distinct(uid)) as uv from test group by key")
-          .show
+      * +---+---+---+
+      * |key|pv |uv |
+      * +---+---+---+
+      * |b  |6  |1  |
+      * |a  |6  |2  |
+      * +---+---+---+
+      * 在流式计算中，可以定义 UDFA 来计算UV，将HyperLogLog2转Array[Byte]存入redis或者hbase每个批次可以提取出来做addAll
+      */
+    df1
+      .union(df1)
+      .groupBy(_._1)
+      .mapPartitions { itor =>
+        itor.map {
+          case (uid, l) =>
+            val list = l.toList
+            var (_, pv, uv) = list.head
+            for (i <- 1 to list.size - 1) {
+              val (_, p, u) = list(i)
+              pv += p
+              uv.addAll(u)
+            }
+            (uid, pv, uv.cardinality())
+        }
+      }
+      .toDF("key", "pv", "uv")
+      .show(false)
+
+    /**
+      * +---+---+---+
+      * |key| pv| uv|
+      * +---+---+---+
+      * |  b|  3|  1|
+      * |  a|  3|  2|
+      * +---+---+---+
+      */
+    spark
+      .sql(
+        s"select key,count(uid) pv,count(distinct(uid)) as uv from test group by key")
+      .show
   }
 }
