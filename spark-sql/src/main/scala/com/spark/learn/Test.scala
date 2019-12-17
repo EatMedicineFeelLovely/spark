3a52c790206e401070cff3a2fb65db03b5b34e3a
mqlin <1356469429@qq.com>
2019-12-17 11:56:41 +0800

    spark sql 一个字段经过udf 变成多个字段：例如 时间戳转 day和hour

--- spark-sql/src/main/scala/com/spark/learn/Test.scala
+++ spark-sql/src/main/scala/com/spark/learn/Test.scala
@@ -1,6 +1,10 @@
 package com.spark.learn
 
+import java.util.Date
+
+import org.apache.commons.lang3.time.DateFormatUtils
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{expr, min, sum}
 
 object Test {
   def main(args: Array[String]): Unit = {
@@ -12,13 +16,40 @@ object Test {
       .getOrCreate()
     import spark.implicits._
 
-    spark.sparkContext.parallelize(
-      Array(("t1", Array("1", "2"),1),
-        ("t1", Array("1", "2","3"),1))
-    ).toDF("table","key","count")
-      .groupBy("table","key")
+    spark.sparkContext
+      .parallelize(
+        Array(("t1", Array("1", "2"), 1), ("t1", Array("1", "2", "3"), 1))
+      )
+      .toDF("table", "key", "count")
+      .groupBy("table", "key")
       .sum("count")
+    // .show
+    registUDF(spark)
+    val df = spark.sparkContext
+      .parallelize(
+        Array("1576552655873")
+      )
+      .toDF("time")
+    df.withColumn("dayhour", expr("getDayHourFromTimestamp(time)"))
+      .withColumn("day", $"dayHour" (0).as("day"))
+      .withColumn("hour", $"dayHour" (1).as("hour"))
       .show
+      // .printSchema()
+  }
+
+  /**
+    *
+    * @param spark
+    */
+  def registUDF(spark: SparkSession): Unit = {
+
+    val getDayHourFromTimestamp = (times: String) => {
+      val t = DateFormatUtils
+        .format(new Date(times.toLong), "yyyyMMdd HH")
+      Array(t.substring(0, 8), t.substring(9, 11))
+    }
+
+    spark.udf.register("getDayHourFromTimestamp", getDayHourFromTimestamp)
 
   }
 }
