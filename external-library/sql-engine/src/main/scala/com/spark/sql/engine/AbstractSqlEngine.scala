2b96b855ca56917d1492d3da59b12b34fd07f9f2
小弓虽 <mqlin@mob.com>
2021-11-22 13:55:13 +0800

    Spark Sql engine

--- external-library/sql-engine/src/main/scala/com/spark/sql/engine/AbstractSqlEngine.scala
+++ external-library/sql-engine/src/main/scala/com/spark/sql/engine/AbstractSqlEngine.scala
@@ -4,28 +4,45 @@ import com.antlr4.parser.{CustomSqlParserLexer, CustomSqlParserParser}
 import com.antrl4.visit.parser.impl.{CustomSqlParserVisitorImpl, ExtErrorListener}
 import com.spark.sql.engine.common.UserDefinedFunction2
 import org.antlr.v4.runtime.{CharStreams, CommonTokenStream}
-import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.expressions.{SparkUserDefinedFunction, UserDefinedFunction}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.ScalaReflection
+import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
 
 import scala.collection.mutable
 import scala.reflect.runtime.universe.TypeTag
+import scala.util.Try
 
 class AbstractSqlEngine(val spark: SparkSession) {
   val udfManager = new mutable.HashMap[String, UserDefinedFunction2]
 
   def register[RT: TypeTag, A1: TypeTag](
                                           name: String,
-                                          func: Function1[A1, RT]): UserDefinedFunction = {
+                                          func: (A1) => RT): UserDefinedFunction = {
+    val outputEncoder = Try(ExpressionEncoder[RT]()).toOption
+    val inputEncoders: Seq[Option[ExpressionEncoder[_]]] = Try(ExpressionEncoder[A1]()).toOption :: Nil
     val udf = spark.udf.register(name, func)
-    udfManager.put(name, UserDefinedFunction2(1, udf.f, udf.dataType, udf.inputTypes))
+    udfManager.put(name, UserDefinedFunction2(1,
+      func,
+      outputEncoder.get.dataTypeAndNullable.dataType,
+      Some(inputEncoders.map(x =>{
+        x.get.dataTypeAndNullable.dataType
+      }))))
     udf
   }
 
   def register[RT: TypeTag, A1: TypeTag, A2: TypeTag](
                                           name: String,
-                                          func: Function2[A1, A2, RT]): UserDefinedFunction = {
+                                          func: (A1, A2) => RT): UserDefinedFunction = {
+    val outputEncoder = Try(ExpressionEncoder[RT]()).toOption
+    val inputEncoders: Seq[Option[ExpressionEncoder[_]]] = Try(ExpressionEncoder[A1]()).toOption :: Nil
     val udf = spark.udf.register(name, func)
-    udfManager.put(name, UserDefinedFunction2(2, udf.f, udf.dataType, udf.inputTypes))
+    udfManager.put(name, UserDefinedFunction2(2,
+      func,
+      outputEncoder.get.dataTypeAndNullable.dataType,
+      Some(inputEncoders.map(x =>{
+        x.get.dataTypeAndNullable.dataType
+      }))))
     udf
   }
 
